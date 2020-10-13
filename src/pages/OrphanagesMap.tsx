fa8e632dfd873d93ca26c09fa7c75b52477d64da
dan9903 <mnssd2@gmail.com>
2020-10-12 22:15:39 -0300

    Creation of map pages and development using env variables

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import mapMakerImg from '../images/map-marker.svg';
+import '../styles/pages/orphanages-map.css';
+import 'leaflet/dist/leaflet.css';
 
 function OrphanagesMap() {
   return (
-    <h1>Hello</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMakerImg} alt="Happy"/>
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita ;)</p>
+        </header>
+        <footer>
+          <strong>Mutuípe</strong>
+          <span>Bahia</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-13.2288796,-39.5174818]}
+        zoom={15}
+        style={{ width: '100%', height: '100%'}}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer 
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+          <FiPlus size="32" color="#fff" />
+      </Link>
+    </div>
   ) 
 }
 
