bf9888e911bb49b72f76a05256584ca4f486e182
Damian Meneses <mnssd2@gmail.com>
2020-10-13 18:40:50 -0300

    46 minutos, banco de dados e orm

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 
-app.get('/users', () => {
-  console.log('Welcome user');
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  return response.json({message: 'Hello world!'});
 });
 
 app.listen(3333);
