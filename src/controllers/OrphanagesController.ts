535f8a28a4311eec841070755dc1fed631afc1a7
Damian Meneses <mnssd2@gmail.com>
2020-10-15 10:27:47 -0300

    validação e cors

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 import OrphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 /* INDEX, SHOW, CREATE, UPDATE, DELETE */
 
@@ -42,8 +43,8 @@ export default {
     const images = requestImages.map( image => {
       return { path: image.filename }
     });
-
-    const orphanage = orphanagesRepository.create({
+    
+    const data = {
       name,
       latitude,
       longitude,
@@ -52,7 +53,26 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+    
+    const schema =  Yup.object().shape({
+      name: Yup.string().required('Nome obrigatório'),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     });
+
+    await schema.validate(data, {abortEarly: false});
+
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
     return response.status(201).json(orphanage);
